ae8520d1928a565d513d399db4fc914713171272
Shobhit Sharma <shobhits377@gmail.com>
2026-08-30 22:17:02 +0530

    Time: 20 ms (13.21%), Space: 47.7 MB (48.03%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,25 +1,28 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         HashMap<Integer, Integer> hm = new HashMap<>();
-        ArrayList<Integer> list = new ArrayList<>();
+        // ArrayList<Integer> list = new ArrayList<>();
         for(int i : nums){
             hm.put(i , hm.getOrDefault(i,0)+1);
         }
-        
+        int[] ans = new int[2];
         for(int i : hm.keySet()){
             if(hm.get(i) == 2){
-                list.add(i);
+                // list.add(i);
+                ans[0] = i;
+
             }
         }
        for(int i = 1; i<=nums.length; i++){
         if(!hm.containsKey(i)){
-            list.add(i);
+            // list.add(i);
+            ans[1] = i;
         }
        }
-       int[] ans = new int[list.size()];
-       for(int i = 0; i<list.size(); i++){
-        ans[i] = list.get(i);
-       }
+    //    int[] ans = new int[list.size()];
+    //    for(int i = 0; i<list.size(); i++){
+    //     ans[i] = list.get(i);
+    //    }
        return ans;
     }
 }
